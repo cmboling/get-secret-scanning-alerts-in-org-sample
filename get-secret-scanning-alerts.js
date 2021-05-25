7d392d20a82e216f6386a97d73488562e1f9f755
Chelsea Boling <cmboling@github.com>
2021-05-25 07:18:20 -0700

    Update get-secret-scanning-alerts.js

--- get-secret-scanning-alerts.js
+++ get-secret-scanning-alerts.js
@@ -7,7 +7,7 @@ const {Octokit} = require('@octokit/rest')
 const octokit = new Octokit({
   auth: process.env.GH_AUTH_TOKEN,
   previews: ['dorian-preview'],
-  baseUrl: process.env.BASE_URL
+  baseUrl: process.env.BASE_URL // example: BASE_URL=https://{hostname}/api/v3
 })
 
 var buffer = ""
